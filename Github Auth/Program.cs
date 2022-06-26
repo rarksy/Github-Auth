abaadfa4332a28f77fadcba70030b6e2b74dc703
Rarksy <corrip2004@gmail.com>
2022-06-26 17:17:17 +1000

    Add project files.

---
+++ Github Auth/Program.cs
@@ -0,0 +1,107 @@
+﻿using static Github_Auth.func;
+using System.Net;
+using System;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Windows;
+
+namespace Github_Auth
+{
+    internal class Program
+    {
+        static string username_input;
+        static string loginASCII = @"
+         ██████╗ ██╗████████╗██╗  ██╗██╗   ██╗██████╗      █████╗ ██╗   ██╗████████╗██╗  ██╗
+        ██╔════╝ ██║╚══██╔══╝██║  ██║██║   ██║██╔══██╗    ██╔══██╗██║   ██║╚══██╔══╝██║  ██║
+        ██║  ███╗██║   ██║   ███████║██║   ██║██████╔╝    ███████║██║   ██║   ██║   ███████║
+        ██║   ██║██║   ██║   ██╔══██║██║   ██║██╔══██╗    ██╔══██║██║   ██║   ██║   ██╔══██║
+        ╚██████╔╝██║   ██║   ██║  ██║╚██████╔╝██████╔╝    ██║  ██║╚██████╔╝   ██║   ██║  ██║
+         ╚═════╝ ╚═╝   ╚═╝   ╚═╝  ╚═╝ ╚═════╝ ╚═════╝     ╚═╝  ╚═╝ ╚═════╝    ╚═╝   ╚═╝  ╚═╝
+        ";
+        static string auth_url = "https://raw.githubusercontent.com/rarksyy/gh_auth_test/main/username.auth";
+
+        [STAThread]
+        static void Main()
+        {
+            log(loginASCII, ConsoleColor.Green);
+            spacer();
+        InitLogin:
+            username_input = input("Enter Username: ");
+
+            using (WebClient client = new WebClient())
+            {
+                Random random = new Random();
+                bool valid_username = client.DownloadString(auth_url+$"?random={random.Next()}").Contains(createmd5(username_input.ToLower()));
+                if (valid_username) login();
+                else {log("Invalid Username", ConsoleColor.Red); goto InitLogin; }
+            }
+        }
+
+        static void login()
+        {
+        InitPassword:
+            string password_input = String.Empty;
+            password_input = input("Enter Password: ", true);
+
+            string CSerial = gethwinfo("Win32_LogicalDisk", "VolumeSerialNumber"),
+            CPUID = gethwinfo("win32_processor", "processorID"),
+            MOBOID = gethwinfo("Win32_BaseBoard", "SerialNumber"),
+            password = createmd5(password_input),
+            reversed = reversestring($"{username_input}{CSerial}{CPUID}{MOBOID}"),
+            md5ified = createmd5(reversed);
+            
+            using (WebClient client = new WebClient())
+            {
+                string usermd5 = createmd5(username_input.ToLower());
+                Random random = new Random();
+                string auth = client.DownloadString(auth_url+"?random="+DateTime.Now.Ticks);
+                if (!auth.Contains(md5ified))
+                {
+                    log(auth);
+                    InvalidAuthInfo(usermd5 + md5ified + password);
+                }
+                string cur;
+                StringReader sr = new StringReader(auth);
+                while ((cur = sr.ReadLine()) != null)
+                {
+                    if (cur.Contains(usermd5+md5ified))
+                    {
+                        if (cur.Length == md5ified.Length)
+                        {
+                            log(auth);
+                            InvalidAuthInfo(usermd5+md5ified+password);
+                            Thread.Sleep(10000);
+                        }
+                        else if (cur.Length > md5ified.Length)
+                        {
+                            if (cur != usermd5 + md5ified + password)
+                            {
+                                log("Invalid Password", ConsoleColor.Red);
+                                goto InitPassword;
+                            }
+                            if (cur.Contains(md5ified + password))
+                            {
+                                Continue();
+                            }
+                            else
+                            {
+                                log("error");
+                                Thread.Sleep(10000);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void Continue()
+        {
+            //successfully logged in, do whatever you want here
+            log("success");
+            Thread.Sleep(10000);
+        }
+    }
+}
---
+++ Github Auth/func.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+using System.Windows;
+using System.Threading;
+
+namespace Github_Auth
+{
+    class func
+    {
+        public static void log<T>(T arg, ConsoleColor Col = ConsoleColor.White) { Console.ForegroundColor = Col; Console.WriteLine($"\n{arg}"); Console.ForegroundColor = ConsoleColor.White; }
+        public static void spacer() { Console.WriteLine(Environment.NewLine); }
+        public static string input(string label = null, bool password = false)
+        {
+            if (!password)
+                { Console.Write(label); return Console.ReadLine(); }
+            else
+            {
+                string pw = string.Empty;
+                Console.Write(label);
+                while (true)
+                {
+                    var key = Console.ReadKey(true);
+                    if (key.Key == ConsoleKey.Enter)                   
+                        return pw;
+                    
+                    if (key.Key == ConsoleKey.Backspace && pw.Length > 0)
+                    {
+                        string _new = pw.Remove(pw.Length - 1);
+                        Console.Write("\x1B[1D");
+                        Console.Write("\x1B[1P");
+                        pw = _new;
+                        continue;
+                    }
+                    if (key.Key != ConsoleKey.Backspace)
+                    {
+                        pw += key.KeyChar;
+                        Console.OutputEncoding = Encoding.Unicode;
+                        Console.Write("*");
+                    }
+                }
+            }
+
+        }
+        public static string createmd5(string input)
+        {
+            using (var md5Hash = System.Security.Cryptography.MD5.Create())
+            {
+                var sourceBytes = Encoding.UTF8.GetBytes(input);
+
+                var hashBytes = md5Hash.ComputeHash(sourceBytes);
+
+                var hash = BitConverter.ToString(hashBytes).Replace("-", string.Empty);
+
+                return hash;
+            }
+        }
+        public static string gethwinfo(string management, string label) 
+        {
+            ManagementClass mgmt = new ManagementClass(management);
+            ManagementObjectCollection moc = mgmt.GetInstances(); 
+            string result = string.Empty;
+            foreach (ManagementObject str in moc) 
+            {
+                if (result == string.Empty)
+                    result += Convert.ToString(str[label]);
+            }
+            return result; 
+        }
+        public static string reversestring(string text)
+        {
+            char[] cArray = text.ToCharArray();
+            string reverse = String.Empty;
+            for (int i = cArray.Length - 1; i > -1; i--)
+            {
+                reverse += cArray[i];
+            }
+            return reverse;
+        }
+        public static void InvalidAuthInfo(string cbdtxt)
+        {
+
+            Clipboard.SetText(cbdtxt);
+            log("Invalid Auth Info, Contact Owner / Support To Validate Credentials.", ConsoleColor.Red);
+            log(cbdtxt);
+            Console.Write("Exiting In 5");
+            for (int i = 5; i >= 0; i--)
+            {
+                Thread.Sleep(1000);
+                Console.Write("\x1B[1D");
+                Console.Write("\x1B[1P");
+                Console.Write(i);
+            }
+            Environment.Exit(0);
+        }
+        public static bool ContainsWord(string s, string word)
+        {
+            string[] ar = s.Split(' ');
+
+            foreach (string str in ar)
+            {
+                if (str.ToLower() == word.ToLower())
+                    return true;
+            }
+            return false;
+        }
+
+    }
+}
